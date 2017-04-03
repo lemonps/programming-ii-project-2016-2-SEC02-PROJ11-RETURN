c6456e7ef45ff9235782a0b0b4fe87e5a6ce02f6
DESKTOP-4733J6F\Lemon <pattharathon.srithundorn@mail.kmutt.ac.th>
2017-04-03 19:08:42 +0700

    all class

---
+++ return_allClass/src/return_allclass/subscription.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package return_allclass;
+
+/**
+ *
+ * @author homun
+ */
+public class subscription {
+    private int id;
+    private int user_id;
+    private int subject_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getSubject_id() {
+        return subject_id;
+    }
+
+    public void setSubject_id(int subject_id) {
+        this.subject_id = subject_id;
+    }
+    
+}
---
+++ return_allClass/src/return_allclass/user.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package return_allclass;
+
+/**
+ *
+ * @author homun
+ */
+public class user {
+    private int id;
+    private String username;
+    private String password;
+    private String name;
+    private String surname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    
+    
+}
